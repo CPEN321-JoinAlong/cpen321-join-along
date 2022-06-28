fc32a9881abaab343a6672d931a0dae90196ad55
Zoeb Gaurani <zoebng@gmail.com>
2022-06-27 21:41:44 -0700

    Added the basic server and database setup

---
+++ backend/models/User.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const UserSchema = new Schema({
+    name: String,
+    interestTags: [
+        {
+            type: String
+        }
+    ],
+    events: [
+        {
+            type: String
+        }
+    ],
+    profileImage: String,
+    description: String,
+    friends: [
+        {
+            type: String
+        }
+    ],
+    blockedUsers: [
+        {
+            type: String
+        }
+    ],
+    blockedEvents: [
+        {
+            type: String
+        }
+    ],
+    token: String
+})
+
+module.exports = mongoose.model("User", UserSchema);

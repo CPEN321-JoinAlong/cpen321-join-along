a8b87fa625c2f5e7c5ef3211b8bab312525acd19
Ken-Liang <ken.liang20@gmail.com>
2022-06-30 00:20:32 -0700

    Initial layout drafts of manage events, chat, and reports.

---
+++ frontend/app/src/main/java/com/joinalongapp/joinalong/ManageEventActivity.java
@@ -0,0 +1,14 @@
+package com.joinalongapp.joinalong;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManageEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manage_event);
+    }
+}

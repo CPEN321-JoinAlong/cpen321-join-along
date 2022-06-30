3729bd8370ae4669781a090d497785862b11d089
jdar8 <justind0237@gmail.com>
2022-06-30 04:20:03 -0700

    implement crud operations

---
+++ frontend/app/src/test/java/com/joinalongapp/controller/RequestManagerTest.java
@@ -0,0 +1,12 @@
+package com.joinalongapp.controller;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class RequestManagerTest {
+    @Test
+    public void placeholderTest(){
+        assertTrue(true);
+    }
+}

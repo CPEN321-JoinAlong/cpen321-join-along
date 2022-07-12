e807149370aa08ed172d552f375b0854951e6d70
jdar8 <justind0237@gmail.com>
2022-07-12 00:56:42 -0700

    refactor message model

--- frontend/app/src/main/java/com/joinalongapp/joinalong/MessageActivity.java
+++ frontend/app/src/main/java/com/joinalongapp/joinalong/MessageActivity.java
@@ -1,17 +1,16 @@
 package com.joinalongapp.joinalong;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import android.os.Bundle;
 
 import com.joinalongapp.adapter.MessageListCustomAdapter;
 import com.joinalongapp.viewmodel.Message;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 public class MessageActivity extends AppCompatActivity {
     private RecyclerView mMessageRecycler;
@@ -23,9 +22,9 @@ public class MessageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_message);
 
         List<Message> lm = new ArrayList<>();
-        Message a = new Message("hello", "Ken", UUID.randomUUID(), 0);
-        Message b = new Message("abc", "yes", UUID.randomUUID(), 1);
-        Message c = new Message("test", "yes", UUID.randomUUID(), 3);
+        Message a = new Message("hello", "Ken", "123", 0);
+        Message b = new Message("abc", "yes", "456", 1);
+        Message c = new Message("test", "yes", "789", 3);
         lm.add(a);
         lm.add(b);
         c.setOwner(true);
--- frontend/app/src/main/java/com/joinalongapp/viewmodel/Message.java
+++ frontend/app/src/main/java/com/joinalongapp/viewmodel/Message.java
@@ -1,43 +1,80 @@
 package com.joinalongapp.viewmodel;
 
-import java.util.UUID;
+import android.util.Log;
 
-public class Message {
-    private String message;
-    private String name;
-    //todo: change to string
-    private UUID id;
-    private long createdAt;
-    private boolean isOwner;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-    public Message(String message, String name, UUID id, long createdAt) {
-        this.message = message;
-        this.name = name;
-        this.id = id;
-        this.createdAt = createdAt;
-    }
-
+public class Message implements IDetailsModel {
     public String getMessage() {
         return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
     }
 
     public String getName() {
         return name;
     }
 
-    public UUID getId() {
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getId() {
         return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
     }
 
     public long getCreatedAt() {
         return createdAt;
     }
 
-    public boolean isOwner(){
+    public void setCreatedAt(long createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public boolean isOwner() {
         return isOwner;
     }
 
     public void setOwner(boolean owner) {
         isOwner = owner;
+    }
+
+    private String message;
+    private String name;
+    //todo: change to string
+    private String id;
+    private long createdAt;
+    private boolean isOwner;
+
+    public Message(String message, String name, String id, long createdAt) {
+        this.message = message;
+        this.name = name;
+        this.id = id;
+        this.createdAt = createdAt;
+    }
+
+//TODO: make sure json keys match with backend
+    @Override
+    public String toJsonString() throws JSONException {
+        JSONObject json = new JSONObject();
+        json.put("message", getMessage());
+        json.put("name", getName());
+        json.put("id", getId());
+        json.put("createdAt", createdAt);
+        json.put("isOwner", isOwner());
+        Log.w("MessageModel", "this may produce unintended results if json keys dont match");
+        return json.toString();
+    }
+
+    @Override
+    public IDetailsModel populateDetailsFromJson(String jsonBody) throws JSONException {
+        return null;
     }
 }

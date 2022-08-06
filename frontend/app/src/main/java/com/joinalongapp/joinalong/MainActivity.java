7eadf9a776176ff13e288b8da874a65107022d07
Ken-Liang <ken.liang20@gmail.com>
2022-08-05 17:49:39 -0700

    fixing test case and some codacy stuff

--- frontend/app/src/main/java/com/joinalongapp/joinalong/MainActivity.java
+++ frontend/app/src/main/java/com/joinalongapp/joinalong/MainActivity.java
@@ -1,13 +1,12 @@
 package com.joinalongapp.joinalong;
 
-import android.annotation.SuppressLint;
-import android.content.Context;
+
 import android.content.Intent;
-import android.content.SharedPreferences;
+
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
+
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
